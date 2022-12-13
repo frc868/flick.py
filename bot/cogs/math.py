5637d5f8ca80fa66d11032ffeaed28adabb17da0
superpenguin612 <74030080+superpenguin612@users.noreply.github.com>
2022-12-13 10:22:08 -0500

    literal quote for pid algo

--- bot/cogs/math.py
+++ bot/cogs/math.py
@@ -28,7 +28,7 @@ class Math(commands.Cog):
     async def pid(self, ctx: commands.Context) -> None:
         embed = tools.create_embed("PID Control Equation")
         url = "https://latex.codecogs.com/png.latex?" + urllib.parse.quote(
-            "\\huge\\dpi{500}\\color{white}" + "u(t) = K_pe(t) + K_i\int_{0}^{t}e(\tau)d\tau + K_d\frac{\mathrm{d} e(t)}{\mathrm{d} t}"
+            "\\huge\\dpi{500}\\color{white}" + r"u(t) = K_pe(t) + K_i\int_{0}^{t}e(\tau)d\tau + K_d\frac{\mathrm{d} e(t)}{\mathrm{d} t}"
         )
         embed.set_image(url=url)
         await ctx.send(embed=embed)
